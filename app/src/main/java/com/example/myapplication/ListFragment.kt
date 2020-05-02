e444ce91ef44997ceb3393ee9ca41d377485a47b
maksim12312314 <maksim.saryev@mail.ru>
2020-05-02 19:23:54 +0300

    dont Work

--- app/src/main/java/com/example/myapplication/ListFragment.kt
+++ app/src/main/java/com/example/myapplication/ListFragment.kt
@@ -66,7 +66,7 @@ class ListFragment:Fragment() {
     }
 
     fun goTo(item:ToDoModel){
-        findNavController().navigate()
+        findNavController().navigate(ListFragmentDirections)
         //val tr = fragmentManager?.beginTransaction()
         // tr?.replace(android.R.id.content, itemFragment.newItem(item.id) )
         //tr?.addToBackStack(null)
